f37dde226e49727ab0b89390f047bf2dc0be97ed
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2012-01-14 23:55:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DadelmoCoffee::Application.config.secret_token = '59bc55d225da56dfefa39a550b1fa1e6bd314bc611bc072e657f6a66c632c074b916cf96db5ecc6b92121868b13cb259cbd66f984267d5031a794c86d9f57840'
