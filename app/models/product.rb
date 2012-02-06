535fc57d00e15041c999a921767f8830c5fcfa41
Daniel <dadelmo@gmail.com>
2012-02-06 10:04:42 +0100

    Some fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :customer_orders
   
   def display_name
-    "#{name} #{price_in_cents.to_f/100}"
+    "#{name} #{price_in_cents.to_f/100}€"
   end
 
 end
