369620398dc41a95d806d344f84b13086444eea7
Daniel <dadelmo@gmail.com>
2012-02-06 10:03:46 +0100

    Some fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
+# encoding: UTF-8
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :customer_orders
   
   def display_name
-    "#{name} #{price_in_cents.to_f/100}€"
+    "#{name} #{price_in_cents.to_f/100}"
   end
 
 end
