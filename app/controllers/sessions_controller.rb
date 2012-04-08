d3af30be2fb6f0bfd3058e27b8728ef08cce7916
Daniel <dadelmo@gmail.com>
2012-04-08 16:03:01 +0200

    Layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ class SessionsController < ApplicationController
 
   def create
     email = params[:session][:email]
+    if email.blank?
+      flash[:error] = 'El email no puede ser vacío'
+      redirect_to root_path
+      return
+    end
     user = User.find_by_email email
     if user.nil?
       user = User.create(:email => email)
+      flash[:success] = "Te has dado de alta ¡Bienvenido #{email}!"
     end
     sign_in user
     redirect_to account_show_path
