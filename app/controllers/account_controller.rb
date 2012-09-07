53bfe78d023aba142abcb183dda2b1640b179d8b
Daniel <dadelmo@gmail.com>
2012-09-07 22:31:03 +0200

    Previous orders

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -4,16 +4,17 @@ class AccountController < ApplicationController
 
     @open_customer_order = nil
     @open_order = nil
+    @prev_orders = []
 
     @customer_orders = @user.customer_orders
     @products = Product.all
 
     @customer_orders.each do |co|
       @open_customer_order = co if !co.order.nil? && co.order.status == 'open'
+      @prev_orders << co if !co.order.nil? && co.order.status != 'open'
     end
 
     @open_order = Order.first(:conditions => {:status => :open}) if @open_customer_order.nil?
-    @prev_orders = Order.all(:conditions => {:status => :open}) if @open_customer_order.nil?
     
   end
 
